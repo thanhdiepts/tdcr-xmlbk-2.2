0f6a1ee5152d08f6da6fb8ee783204244b504000
thanhdiepts <ldtdiep@gmail.com>
2015-12-09 16:14:30 +0700

    Update and rename initcloudnk.sh to initcloudbk.sh

--- root/initcloudbk.sh
+++ root/initcloudbk.sh
@@ -5,6 +5,6 @@ fetch https://raw.githubusercontent.com/thanhdiepts/tdcr-xmlbk-2.2/master/root/c
 fetch https://raw.githubusercontent.com/thanhdiepts/tdcr-xmlbk-2.2/master/root/_if_xml.php
 fetch https://raw.githubusercontent.com/thanhdiepts/tdcr-xmlbk-2.2/master/root/initcloudnk.sh
 chmod +x /root/checkip.sh /root/cloudbk.sh /root/initcloudnk.sh
-ln -sf /root/cloudbk.sh /usr/local/etc/rc.d/initcloudnk
+ln -sf /root/cloudbk.sh /usr/local/etc/rc.d/initcloudbk
 ln -sf /root/cloudbk.sh /usr/local/etc/rc.d/cloudbk
 sh /root/checkip.sh && sh /root/cloudbk.sh 
