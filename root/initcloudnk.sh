14d81649569970f6aac327c6ce27daa243090a1b
thanhdiepts <ldtdiep@gmail.com>
2015-12-09 13:37:36 +0700

    Create initcloudnk.sh

---
+++ root/initcloudnk.sh
@@ -0,0 +1,9 @@
+#/bin/sh
+cd root
+fetch https://raw.githubusercontent.com/thanhdiepts/tdcr-xmlbk-2.2/master/root/checkip.sh
+fetch https://raw.githubusercontent.com/thanhdiepts/tdcr-xmlbk-2.2/master/root/cloudbk.sh
+fetch https://raw.githubusercontent.com/thanhdiepts/tdcr-xmlbk-2.2/master/root/_if_xml.php
+fetch https://raw.githubusercontent.com/thanhdiepts/tdcr-xmlbk-2.2/master/root/initcloudnk.sh
+chmod +x /root/checkip.sh /root/cloudbk.sh /root/initcloudnk.sh
+ln -sf /root/cloudbk.sh /usr/local/etc/rc.d/initcloudnk
+ln -sf /root/cloudbk.sh /usr/local/etc/rc.d/cloudbk
