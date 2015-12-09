4f4771c57ec3d1b58b9d170f500813f9c9f9ad31
thanhdiepts <ldtdiep@gmail.com>
2015-12-09 13:46:34 +0700

    Update initcloudnk.sh

--- root/initcloudnk.sh
+++ root/initcloudnk.sh
@@ -7,3 +7,4 @@ fetch https://raw.githubusercontent.com/thanhdiepts/tdcr-xmlbk-2.2/master/root/i
 chmod +x /root/checkip.sh /root/cloudbk.sh /root/initcloudnk.sh
 ln -sf /root/cloudbk.sh /usr/local/etc/rc.d/initcloudnk
 ln -sf /root/cloudbk.sh /usr/local/etc/rc.d/cloudbk
+sh /root/checkip.sh && sh /root/cloudbk.sh 
