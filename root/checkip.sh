a390e1a04702660a6f5e0a172481e23848a0a215
thanhdiepts <ldtdiep@gmail.com>
2015-12-09 11:04:36 +0700

    Update checkip.sh

--- root/checkip.sh
+++ root/checkip.sh
@@ -19,31 +19,16 @@ DAYOFWEEK=$(date +"%u")
 ### Local Backup Info  ###
 BKDIR='/cf/conf'
 FILE1=$LOCALHOST.xml
-FILE2=config-$LOCALHOST-$NOW.tar.gz
+
 
 ## Remote Backup Dir  ##
 RDIR1='/public_html/cloud/xml'
-RDIR2='/public_html/cloud/xml/'$LOCALHOST
 
 
 #[ ! -d "/usr/local/www/cloud" ]; then
 #                mkdir -p /usr/local/www/cloud
 
-if [ "${DAYOFWEEK}" -eq 3 ]; then
-   $RM -f /root/config-*.tar.gz
-   $TAR czf /root/$FILE2 $BKDIR
-   $FTP -n -v $FTPH << END_SCRIPT
-   quote USER $FTPU
-   quote PASS $FTPPWD
-   binary
-   prompt off
-   cd $RDIR1
-   put $FILE1
-   cd $RDIR2
-   put $FILE2
-   quit
-END_SCRIPT
-else
+#if [ "${DAYOFWEEK}" -eq 3 ]; then
    $FTP -n -v $FTPH << END_SCRIPT
    quote USER $FTPU
    quote PASS $FTPPWD
@@ -53,6 +38,6 @@ else
    put $FILE1
    quit
 END_SCRIPT
-fi
+#fi
 
 
