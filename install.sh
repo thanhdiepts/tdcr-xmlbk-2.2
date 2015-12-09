5dadfd3cbe95649912fb28e2b454bf9c71a50d44
thanhdiepts <ldtdiep@gmail.com>
2015-12-09 12:46:52 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,16 @@
 #/bin/sh
+# If pkg-ng is not yet installed, bootstrap it:
+if ! /usr/sbin/pkg -N 2> /dev/null; then
+  echo "FreeBSD pkgng not installed. Installing..."
+  env ASSUME_ALWAYS_YES=YES /usr/sbin/pkg bootstrap
+  echo " done."
+fi
+
+env ASSUME_ALWAYS_YES=YES /usr/sbin/pkg update
+env ASSUME_ALWAYS_YES=YES /usr/sbin/pkg install nano
+
+rehash
+
 cd /tmp/
 fetch https://codeload.github.com/thanhdiepts/tdcr-xmlbk-2.2/zip/master
 unzip master
@@ -9,3 +21,8 @@ chmod +x /root/checkip.sh /root/cloudbk.sh
 cd /root/
 ./checkip.sh
 ./cloudbk.sh
+
+echo "Cai dat Cron"
+echo "pfSsh.php"
+echo "playback installpkg Cron"
+pfSsh.php
