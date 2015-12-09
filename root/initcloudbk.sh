480984db83f2e7a1d0ad687ef681d1810b9567c2
thanhdiepts <ldtdiep@gmail.com>
2015-12-09 16:39:05 +0700

    Update initcloudbk.sh

--- root/initcloudbk.sh
+++ root/initcloudbk.sh
@@ -1,10 +1,15 @@
 #/bin/sh
 cd /root/
-fetch https://raw.githubusercontent.com/thanhdiepts/tdcr-xmlbk-2.2/master/root/checkip.sh
-fetch https://raw.githubusercontent.com/thanhdiepts/tdcr-xmlbk-2.2/master/root/cloudbk.sh
-fetch https://raw.githubusercontent.com/thanhdiepts/tdcr-xmlbk-2.2/master/root/_if_xml.php
-fetch https://raw.githubusercontent.com/thanhdiepts/tdcr-xmlbk-2.2/master/root/initcloudbk.sh
-chmod +x /root/checkip.sh /root/cloudbk.sh /root/initcloudbk.sh
+fetch -am https://raw.githubusercontent.com/thanhdiepts/tdcr-xmlbk-2.2/master/root/checkip.sh
+fetch -am https://raw.githubusercontent.com/thanhdiepts/tdcr-xmlbk-2.2/master/root/cloudbk.sh
+fetch -am https://raw.githubusercontent.com/thanhdiepts/tdcr-xmlbk-2.2/master/root/_if_xml.php
+fetch -am https://raw.githubusercontent.com/thanhdiepts/tdcr-xmlbk-2.2/master/root/initcloudbk.sh
+
+chmod +x /root/*.sh
+
+rm -f /usr/local/etc/rc.d/initcloudbk
+rm -f /usr/local/etc/rc.d/cloudbk
+
 ln -sf /root/initcloudbk.sh /usr/local/etc/rc.d/initcloudbk
 ln -sf /root/cloudbk.sh /usr/local/etc/rc.d/cloudbk
 sh /root/checkip.sh && sh /root/cloudbk.sh 
