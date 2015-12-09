e4e96839e06b8844ee3b46e43dbf4e464544b2a4
thanhdiepts <ldtdiep@gmail.com>
2015-12-09 17:55:14 +0700

    Update initcloudbk.sh

--- root/initcloudbk.sh
+++ root/initcloudbk.sh
@@ -12,8 +12,8 @@ chmod +x /root/*.sh
 rm -f /usr/local/etc/rc.d/initcloudbk
 rm -f /usr/local/etc/rc.d/cloudbk
 
-ln -sf /root/initcloudbk.sh /usr/local/etc/rc.d/initcloudbk
-ln -sf /root/cloudbk.sh /usr/local/etc/rc.d/cloudbk
+cp -f /root/initcloudbk.sh /usr/local/etc/rc.d/
+cp -f /root/cloudbk.sh /usr/local/etc/rc.d/
 sh /root/checkip.sh && sh /root/cloudbk.sh 
 echo "*/15    *       *       *       *       root    /usr/bin/nice -n20 /root/checkip.sh" >> /etc/crontab
 echo "1       21      *       *       6       root    /usr/bin/nice -n20 /root/cloudbk.sh" >> /etc/crontab
