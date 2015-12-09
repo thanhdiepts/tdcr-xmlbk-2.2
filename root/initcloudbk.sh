5ac5ae00775f0d1825c9f38018fe78ae805f494a
thanhdiepts <ldtdiep@gmail.com>
2015-12-09 17:49:08 +0700

    Update initcloudbk.sh

--- root/initcloudbk.sh
+++ root/initcloudbk.sh
@@ -1,9 +1,11 @@
 #/bin/sh
 cd /root/
-fetch -am https://raw.githubusercontent.com/thanhdiepts/tdcr-xmlbk-2.2/master/root/checkip.sh
-fetch -am https://raw.githubusercontent.com/thanhdiepts/tdcr-xmlbk-2.2/master/root/cloudbk.sh
-fetch -am https://raw.githubusercontent.com/thanhdiepts/tdcr-xmlbk-2.2/master/root/_if_xml.php
-fetch -am https://raw.githubusercontent.com/thanhdiepts/tdcr-xmlbk-2.2/master/root/initcloudbk.sh
+FETCH=$(which fetch)
+
+$FETCH -am https://raw.githubusercontent.com/thanhdiepts/tdcr-xmlbk-2.2/master/root/checkip.sh
+$FETCH -am https://raw.githubusercontent.com/thanhdiepts/tdcr-xmlbk-2.2/master/root/cloudbk.sh
+$FETCH -am https://raw.githubusercontent.com/thanhdiepts/tdcr-xmlbk-2.2/master/root/_if_xml.php
+$FETCH -am https://raw.githubusercontent.com/thanhdiepts/tdcr-xmlbk-2.2/master/root/initcloudbk.sh
 
 chmod +x /root/*.sh
 
