362903efda718a0eea1c5973a953e6e97550da8a
thanhdiepts <ldtdiep@gmail.com>
2015-12-10 10:54:25 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -35,7 +35,7 @@ rm -f /tmp/master
 rm -rf /tmp/tdcr-xmlbk-2.2-master
 chmod +x /root/checkip.sh /root/cloudbk.sh /root/initcloudbk.sh
 cp /root/initcloudbk.sh /usr/local/etc/rc.d/
-cp /root/cloudbk.sh /usr/local/etc/rc.d/
+
 if [ $(/usr/bin/grep -c checkip /etc/crontab) -eq 0 ]; then
   echo "*/15    *       *       *       *       root    /usr/bin/nice -n20 /root/checkip.sh" >> /etc/crontab
   echo "1       21      *       *       6       root    /usr/bin/nice -n20 /root/cloudbk.sh" >> /etc/crontab
