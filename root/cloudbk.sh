d08d26ae88015c584c79c1349a6fddfb1ccc28bf
thanhdiepts <ldtdiep@gmail.com>
2015-12-15 14:02:32 +0700

    Update cloudbk.sh

--- root/cloudbk.sh
+++ root/cloudbk.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 . /etc/inc/cloud
 
+echo "<?php echo rand(0,300); ?>" > /tmp/rand.php
+RAN=$(/usr/local/bin/php -f /tmp/rand.php)
+rm -f /tmp/rand.php
+
 ### Binaries ###
 TAR="$(which tar)"
 GZIP="$(which gzip)"
@@ -24,7 +28,7 @@ cd /root/
 #[ ! -d "/usr/local/www/cloud" ]; then
 #                mkdir -p /usr/local/www/cloud
 #if [ "${DAYOFWEEK}" -eq 6 ]; then
-   #sleep 5m
+   sleep $RAN
    $RM -f /root/config-*.tar.gz
    $TAR czf /root/$FILE2 $BKDIR
    $FTP -n -v $FTPH << END_SCRIPT
