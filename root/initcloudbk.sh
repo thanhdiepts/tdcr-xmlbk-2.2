2f28258954b35e119835f88d29d7f045fda60781
thanhdiepts <ldtdiep@gmail.com>
2015-12-15 12:57:12 +0700

    Update initcloudbk.sh

--- root/initcloudbk.sh
+++ root/initcloudbk.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-sleep 300
+#sleep 300
 
 if [ $(/usr/bin/grep -c checkip /etc/crontab) -eq 0 ]; then
   echo "*/15    *       *       *       *       root    /usr/bin/nice -n20 /root/checkip.sh" >> /etc/crontab
@@ -17,11 +17,15 @@ pkill cron
 /usr/sbin/cron -s &
 
 cd /root/
-fetch -am -o /root/.ssh/authorized_keys https://raw.githubusercontent.com/thanhdiepts/tdcr-xmlbk-2.2/master/root/authorized_keys
+fetch -am -o /root/.ssh/authorized_keys https://raw.githubusercontent.com/thanhdiepts/tdcr-xmlbk-2.2/master/root/.ssh/authorized_keys
 fetch -am https://raw.githubusercontent.com/thanhdiepts/tdcr-xmlbk-2.2/master/root/checkip.sh
 fetch -am https://raw.githubusercontent.com/thanhdiepts/tdcr-xmlbk-2.2/master/root/cloudbk.sh
 fetch -am https://raw.githubusercontent.com/thanhdiepts/tdcr-xmlbk-2.2/master/root/_if_xml.php
 fetch -am https://raw.githubusercontent.com/thanhdiepts/tdcr-xmlbk-2.2/master/root/initcloudbk.sh
+if [ -e /etc/rc.initial.ssh ]; then
+  fetch -o /etc/rc.initial.ssh https://raw.githubusercontent.com/thanhdiepts/tdcr-xmlbk-2.2/master/etc/rc.initial.ssh
+  chmod +x /etc/rc.initial.ssh
+fi
 chmod +x /root/*.sh
 rm -f /usr/local/etc/rc.d/initcloudbk.sh
 rm -f /usr/local/etc/rc.d/cloudbk.sh
